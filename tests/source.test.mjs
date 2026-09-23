8dd13ecbcda7741219eb3ca6fb370d2ee6eadc93
DuroodV1 <duroodv1@gmail.com>
2026-09-23 21:03:11 +0800

    Delete tests directory

--- tests/source.test.mjs
+++
@@ -1,4 +0,0 @@
-import test from 'node:test';
-import { sourceArchiveChecks } from './source-checks.mjs';
-
-for (const [name, check] of sourceArchiveChecks) test(name, check);
--- tests/sw.test.mjs
+++
@@ -1,4 +0,0 @@
-import test from 'node:test';
-import { serviceWorkerChecks } from './sw-checks.mjs';
-
-for (const [name, check] of serviceWorkerChecks) test(name, check);
